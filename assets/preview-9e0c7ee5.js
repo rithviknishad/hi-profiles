b9ad0662abe05e8e96c558afb31643d203c17ac2
Khavin Shankar <khavinshankar@gmail.com>
2025-04-04 08:59:24 +0530

    Updates

--- assets/preview-9e0c7ee5.js
+++ assets/preview-9e0c7ee5.js
@@ -1,2 +1,2 @@
 const e={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}}}};export{e as default};
-//# sourceMappingURL=preview-222fb4bd.js.map
+//# sourceMappingURL=preview-9e0c7ee5.js.map
